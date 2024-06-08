08f2389e19a97427823388343d6f88e3e14ae100
LukaszK <lukasz.korycinski@gmail.com>
2024-06-08 11:54:23 +0200

    h

--- app/src/main/java/com/example/neuralnetworkkotlin/gameLogic/strategy/StrategyGame.kt
+++ app/src/main/java/com/example/neuralnetworkkotlin/gameLogic/strategy/StrategyGame.kt
@@ -23,7 +23,7 @@ class StrategyGame(val file3DA: File3dA, val file3Df: File3d, val textures: Text
     fun onClick(motionEvent: MotionEvent, pos: Vector2f) {
         val pointer3d = camera.unproject(pos)
 
-        humans.onClick(motionEvent, pos)
+        humans.onClick(motionEvent, pointer3d)
 
         Pointer.position = pointer3d
     }
